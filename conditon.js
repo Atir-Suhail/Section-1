21b20313164fc42c811b9e7c96f3cdcf643941cb
Atir-Suhail <atirsuhail406@gmail.com>
2024-06-29 17:23:17 +0530

    Today, I Learned Basics of JavaScripts..

---
+++ conditon.js
@@ -0,0 +1,19 @@
+n = 344;
+if(n%2==0){
+    console.log('Even');
+    something = 'Hello';
+    let x = 'nice';
+    const pi = 3.1416;
+   
+}else{
+    console.log('ODD');
+}
+console.log(something);
+isLogin = true;
+console.log( isLogin );
+// console.log(x);
+// exponent operator
+console.log( 2 ** 4 );
+console.log( 11 + '11');
+console.log( 11 + 22 + '11');
+console.log(typeof isLogin)
---
+++ index.js
@@ -0,0 +1,17 @@
+console.log('Hello JavaScript');
+
+console.log(' I am Learning JavaScript in Digipodium');
+
+a = 451;
+
+console.log(a);
+console.log(typeof a);
+
+myname = 'Atir';
+
+console.log(myname);
+console.log(typeof myname);
+
+isLogin = true;
+console.log(isLogin)
+console.log(typeof isLogin)
