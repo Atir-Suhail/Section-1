30cfa723cefe68eb82b6b8ab782dfa7823d52f2a
Atir-Suhail <atirsuhail406@gmail.com>
2024-07-01 17:26:27 +0530

    Basics of JS

--- array.js
+++ array.js
@@ -1,5 +1,36 @@
-const arr = [65,'nice',console.log,undefined,true];
-console.log(arr);
-console.log(typeof arr)
-console.log(Array.isArray(arr));
-console.log(arr.length);
+// Indexing
+const movies = ['300','Mission Impossible','Deadpool','Expendables','3 Idiots','Inception', ];
+console.log( movies[3] );
+console.log(movies.at(-2));
+console.log(movies.indexOf('300'));
+movies[2] = 'Deadpool & Wolverine';
+console.log(movies);
+
+// Slicing
+console.log(movies.slice(2));
+console.log(movies.slice(-4));
+console.log(movies.slice(-4,));
+console.log(movies.slice(-4,-2));
+console.log(movies.slice(-7,-2));
+console.log(movies.slice());
+
+
+// Length Of String
+console.log('Atir'.length);
+console.log(movies[2].slice(-9));
+
+// Adding and Removing of ELements
+
+movies.push('Kanchana');
+console.log(movies);
+
+movies.unshift('Gangs of Wasseypur');  // adds elements at the beginning
+console.log(movies);
+
+console.log(movies.pop()); //remove elements at the end
+console.log(movies.shift());// remove elements at the beginning
+console.log(movies);
+// movies,splice is used for removing 2 elements from index 3;
+movies.splice(3,0, 'The Boys','Incidious');
+console.log(movies);
+
---
+++ arrayOperations.js
@@ -0,0 +1,5 @@
+const fruits =['Mango','Banana','Apple','Orange','Pineapple','Grapes','Papaya'];
+console.log(fruits);
+const index = fruits.indexOf('Orange');
+console.log(fruits.indexOf('Papaya'));
+console.log(fruits.slice(index));
