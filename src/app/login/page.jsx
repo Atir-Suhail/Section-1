997a8ab9ffc76d3efcd13f97bbbe06254ab30c96
Atir-Suhail <atirsuhail406@gmail.com>
2024-07-10 23:36:23 +0530

    Basics of React

---
+++ src/app/login/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import classes from './login.module.css'
+
+const login = () => {
+  return (
+    <div>
+      <h1 className='text -3xl text-center mt-10'>Login Page</h1>
+    <button className={classes.loginBtn}>Login Now</button>
+    </div>
+  )
+}
+
+export default login;
