d0045015cb84d8d07bc46b2c55ce25cd0f800b21
Atir-Suhail <atirsuhail406@gmail.com>
2024-07-21 23:58:07 +0530

    index.js

--- index.js
+++ index.js
@@ -5,10 +5,12 @@ console.log(' I am Learning JavaScript in Digipodium');
 a = 451;
 
 
+
 console.log(a);
 console.log(typeof a);
 
 myname = 'Atir';
+
 
 console.log(myname);
 console.log(typeof myname);
